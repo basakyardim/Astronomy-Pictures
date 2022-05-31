3ddf042d1fac0f85820dff521192b32898e5968f
basakyardim <basakyrdm@gmail.com>
2022-05-31 16:33:02 +0300

    Added models, api structure, dependencies

---
+++ app/src/main/java/com/basakyardim/marsrovers_apod/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.basakyardim.marsrovers_apod.common
+
+object Constants {
+    const val API_KEY = "qdreF5RVR82fs934bhxslaIP2BcgqBxKDLPA4vWB"
+}
---
+++ app/src/main/java/com/basakyardim/marsrovers_apod/domain/model/Apod.kt
@@ -0,0 +1,10 @@
+package com.basakyardim.marsrovers_apod.domain.model
+
+data class Apod(
+    val copyright: String,
+    val date: String,
+    val explanation: String,
+    val hdurl: String,
+    val title: String,
+    val url: String
+)
